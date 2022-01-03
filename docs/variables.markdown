08d64d070b7e87b58714e8e174105d42896a1c10
Liam Galvin <liam@liam-galvin.co.uk>
2022-01-03 17:44:48 +0000

    Add more base variables (#24)

--- docs/variables.markdown
+++ docs/variables.markdown
@@ -10,6 +10,9 @@ nav_order: 5
 |--------------------|-------------|
 | user_config_dir    | `$XDG_CONFIG_HOME` if it exists, otherwise `~/.config`.
 | user_home_dir      | Current user's home directory.
+| sys_os             | Operating system, e.g. `linux`, `macos`, `windows`
+| sys_distro         | Linux distribution e.g. `ubuntu`, `arch` etc. Empty if non-Linux
+| sys_arch           | System architecture, e.g. amd64
 
 Variables for a module come from the module defaults, the parent modules variables for that module, or from the global overrides.
 
--- internal/pkg/config/variables.go
+++ internal/pkg/config/variables.go
@@ -3,6 +3,7 @@ package config
 import (
 	"os"
 
+	"github.com/liamg/peridot/internal/pkg/system"
 	"github.com/liamg/peridot/internal/pkg/variable"
 )
 
@@ -11,8 +12,13 @@ func BaseVariables() variable.Collection {
 	configDir, _ := configRoot()
 	homeDir, _ := os.UserHomeDir()
 
+	info := system.Info()
+
 	return variable.NewCollection(map[string]interface{}{
 		"user_home_dir":   homeDir,
 		"user_config_dir": configDir,
+		"sys_os":          info.OperatingSystem,
+		"sys_distro":      info.Distribution,
+		"sys_arch":        info.Architecture,
 	})
 }
